de23e7cf91aec050cc877684ddc09026c2ddfd0f
Егор Борушкин <redbuuuulle1@gmail.com>
2024-12-28 02:53:55 +0300

    ggg

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -8,30 +8,30 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
     path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
     path(
-        'posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'
+        'posts/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'
     ),
     path(
-        'posts/<int:pk>/edit/',
+        'posts/<int:post_id>/edit/',
         views.PostUpdateView.as_view(),
         name='edit_post',
     ),
     path(
-        'posts/<int:pk>/delete/',
+        'posts/<int:post_id>/delete/',
         views.PostDeleteView.as_view(),
         name='delete_post',
     ),
     path(
-        'posts/<int:pk>/comment/',
+        'posts/<int:post_id>/comment/',
         views.CommentCreateView.as_view(),
         name='add_comment',
     ),
     path(
-        'posts/<int:pk>/edit_comment/<int:comment_id>',
+        'posts/<int:post_id>/edit_comment/<int:comment_id>',
         views.CommentUpdateView.as_view(),
         name='edit_comment',
     ),
     path(
-        'posts/<int:pk>/delete_comment/<int:comment_id>',
+        'posts/<int:post_id>/delete_comment/<int:comment_id>',
         views.CommentDeleteView.as_view(),
         name='delete_comment',
     ),
